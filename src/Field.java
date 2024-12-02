6d7b5f7bd2c5a74ea536919ab1eec9be44a17457
qiulorie <lorieqiu04@gmail.com>
2024-12-02 11:41:47 +0100

    Correction blocks

--- src/Field.java
+++ src/Field.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 public class Field {
     public static final int ALTITUDE_GAP = 80;
-    public static final int START_ALTITUDE = 300;
+    public static final int START_ALTITUDE = 40;
 
     public final int width, height;
     private int bottom, top; // bottom and top altitude
